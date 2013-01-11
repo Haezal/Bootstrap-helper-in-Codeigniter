46299befb89b0cc1e57043b85ffd646d3b372857
haezal <ezalepy@gmail.com>
2013-01-11 17:08:20 +0800

    Update MY_form_helper.php

--- MY_form_helper.php
+++ MY_form_helper.php
@@ -1,7 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-// created by haezal musa 
+
 // ============================================================================
 
 
